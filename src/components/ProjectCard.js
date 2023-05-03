6e0617529413bae8d82423e702bc4ef5081ba207
Reuben White <white.reuben@gmail.com>
2023-05-03 12:48:46 +1200

    updated projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import aboutImg from "../assets/20190816_193930.jpg";
 
-const URLShortener = () => {
+const Chatty = () => {
   return (
     <div className="bg-[#212121]">
       <div className="flex max-w-[1100px] w-full pt-[115px] height-auto flex-col gap-10 xl:flex-row items-center justify-center m-auto  ">
@@ -42,7 +42,7 @@ const URLShortener = () => {
   );
 };
 
-const Chatty = () => {
+const URLShortener = () => {
   return (
     <div className="bg-[#1c6f7c]">
       <div className="flex max-w-[1100px] w-full pt-[150px] height-auto flex-col gap-10 xl:flex-row-reverse items-center justify-center m-auto  ">
@@ -58,21 +58,16 @@ const Chatty = () => {
             Project Name
           </h3>
           <p className="w-screen xl:w-[calc(40vw-20px)] text-xl tracking-wide leading-relaxed text-white text-left">
-            Hi there, thanks for checking out my Portfolio! I'm an experienced
-            techie with over 14 years experience in the field. After selling my
-            business last year I decided to move into Software Engineering. The
-            rest of my time in IT has been spent in VOIP, Systems and Network
-            Engineering so I have a wide ranging knowledge, and I've now topped
-            it off with the ability to make things from scratch
+            A URL shortener similar to bit.ly. Stores shortened URLs in a MongoDB. This was a project with a pre-built frontend (thanks to <a href="https://maximorlov.com/backend-portfolio-projects-no-css">Maxim Orlov</a>). The backend was built from scratch based a list of features.
           </p>
           <div className="flex gap-5 m-auto">
-            <a href="https://www.github.com" target="_blank" className="m-auto">
+            <a href="https://www.github.com/watchmego/url-shortener" target="_blank" className="m-auto">
               <button class="bg-transparent hover:white text-white hover:text-black hover:bg-white font-semibold py-2 px-4 border border-white hover:border-black rounded">
                 Github
               </button>
             </a>
             <a
-              href="https://www.reubenwhite.nz/url"
+              href="https://urlsh.reubenwhite.nz"
               target="_blank"
               className="m-auto"
             >
